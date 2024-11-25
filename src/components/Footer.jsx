c1e1b85c50b16a57115c83a6dd4bf61471207b0e
moeezkhan1 <bscs21040@itu.edu.pk>
2024-11-25 23:40:54 +0500

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <ul>
+      <li>Support</li>
+      <li>Community</li>
+      <li>Hosting</li>
+      <li>About</li>
+    </ul>
+    <p>&copy; 2024 Airbnb, Inc.</p>
+  </footer>
+);
+
+export default Footer;
